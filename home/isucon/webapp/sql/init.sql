c6b4289e9899a73452741fce80e12c347c404526
m0riiiii <rewop.ecnalab.x2@gmail.com>
2023-11-25 15:17:12 +0900

    add index for livestream releation

--- home/isucon/webapp/sql/init.sql
+++ home/isucon/webapp/sql/init.sql
@@ -29,3 +29,12 @@ ALTER TABLE ng_words ADD INDEX ng_words_live_user(livestream_id, user_id);
 
 ALTER TABLE livestream_tags DROP INDEX livestrmtag_live;
 ALTER TABLE livestream_tags ADD INDEX livestrmtag_live(livestream_id);
+
+ALTER TABLE livestream DROP INDEX livestream_userid;
+ALTER TABLE livestream ADD INDEX livestream_userid(user_id);
+
+ALTER TABLE reaction DROP INDEX reaction_livestrmid;
+ALTER TABLE reaction ADD INDEX reaction_livestrmid(livestream_id);
+
+ALTER TABLE livecomments DROP INDEX livecomments_livestrmid;
+ALTER TABLE livecomments ADD INDEX livecomments_livestrmid(livestream_id);
